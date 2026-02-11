1db205e5e139dc8a690bd6fd33540dc25fc49437
yolo1-rq <15288329721@163.com>
2026-02-12 00:02:22 +0800

    five

--- chapter2/2-3-2.py
+++ chapter2/2-3-2.py
@@ -2,3 +2,9 @@ first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 print(full_name)
+print(full_name.title())
+print(full_name.upper())
+print(full_name.lower())
+print(f"hello,thank,{full_name.title()}")  
+message = f"hello,thank,{full_name.title()}"
+print(message)
